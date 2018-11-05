2a3dd651b1610f4cbb7a970e045f0885a090d9ff
Akili Humphrey <akilihumphrey@gmail.com>
2018-11-05 08:50:11 +0000

    automatically backed up by learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,7 +1,6 @@
 sentence1 = "Hello there, and how are you?"
 def reverse_each_word(sentence1)
-sentence1.split
-sentence1.reverse.each.word do |item|
+sentence1.split do |item|
   puts "#{item}"
 end
 end
