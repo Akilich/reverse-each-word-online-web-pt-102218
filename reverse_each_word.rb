d7963de4b437d54fa6d8523a9a832fd4a9437e06
Akili Humphrey <akilihumphrey@gmail.com>
2018-11-05 09:02:01 +0000

    automatically backed up by learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,6 +1,21 @@
-sentence1 = "Hello there, and how are you?"
-def reverse_each_word(sentence1)
-sentence1.each do |item|
-  puts "#{item}"
+sentence1 = string.split(" ")
+  return_array = []
+  sentence1.each do|string|
+    return_array << string.reverse
+  end
+  return_array.join(" ")
 end
+
+def reverse_each_word(string)
+  array = string.split(" ") #turn string into an array
+  test_array = []
+  array.collect do|string| #iterate over the array
+    test_array << string.reverse #reverse each word in the array
+  end
+  test_array.join(" ")
 end
+
+
+reverse_each_word("Hello there, and how are you?")
+  #=> "olleH ,ereht dna woh era ?uoy"
+  
